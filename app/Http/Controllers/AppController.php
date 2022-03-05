e43d23fa03d79980330abccfe2cc4181359a6569
Houcem Hedhly <hedhoucem@gmail.com>
2022-03-05 11:20:57 +0100

    create a controller to handle route

---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class AppController extends Controller
+{
+    public function welcome()
+    {
+        $todos = Todo::all();
+        return view('welcome', ['todos' => $todos]);
+    }
+}
