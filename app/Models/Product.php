9ea255360d9af224a19a4993d6710300437b742d
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-05 22:41:32 +0100

    create Product relation to category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function categories(){
+        return $this->belongsTo(Category::class);
+    }
 }
