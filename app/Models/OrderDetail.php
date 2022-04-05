fdaa0440d15d986adf50bab2b1ff9fce68f5179c
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-05 22:41:17 +0100

    Create orderDetail relation to order and product

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
