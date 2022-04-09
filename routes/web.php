2b7c1a58c15f24115400e37845aa1ed880d1159c
houcem <hedhoucem@gmail.com>
2022-04-09 11:55:49 +0100

    create PostController with resources

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [AppController::class, 'welcome']);
+
+// // Read
+// Route::get('/posts', [PostController::class, 'allPosts']);
+// Route::get('/posts/{id}', [PostController::class, 'getPost']);
+// // Create
+// Route::get('/posts/create', [PostController::class, 'createPostForm']);
+// Route::post('/posts', [PostController::class, 'createPost']);
+// // Update
+// Route::get('/posts/{id}/edit', [PostController::class, 'editPostForm']);
+// Route::put('/posts/{id}', [PostController::class, 'editPost']);
+// // Delete
+// Route::delete('/posts/{id}', [PostController::class, 'deletePost']);
+
+Route::resource('/posts', PostController::class);
