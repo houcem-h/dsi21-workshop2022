ffcd27c77d7ee439183edb1fd8465e38124e34f6
houcem <hedhoucem@gmail.com>
2022-04-15 13:50:23 +0100

    add a name prefix to the api routes

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/posts', PostController::class);
+Route::name('api.')->group(function () {
+    Route::apiResource('/post', PostController::class);
+});
