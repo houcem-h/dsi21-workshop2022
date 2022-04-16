17a6644e5edc97bdae6b691f81e57c73aa1c1372
houcem <hedhoucem@gmail.com>
2022-04-16 14:32:58 +0100

    query relationship

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::with('category')->get();
         return view('posts.index', compact('posts'));
     }
 
