5889e85144502372be73c137bb2d4649325fa8c7
houcem <hedhoucem@gmail.com>
2022-05-13 16:16:21 +0100

    login with for SPA

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::apiResource('/posts', PostController::class);
 });
 
-Route::apiResource('/posts', PostController::class);
+Route::post('/login', [AuthController::class, 'login']);
