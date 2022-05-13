a0941ba0336a1822e5dbf866ea75c6a9b3829237
houcem <hedhoucem@gmail.com>
2022-05-13 16:00:22 +0100

    create login method in AuthController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Check email
+        $user = User::where('email', $request['email'])->first();
+
+        // Check password
+        if (!$user || !Hash::check($request['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+}
