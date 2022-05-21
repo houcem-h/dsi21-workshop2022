1eb5181c35745ccfa03bc9c31451c3444b9dda8d
houcem <hedhoucem@gmail.com>
2022-05-21 15:40:44 +0100

    fix errors before fire login method

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -2,42 +2,40 @@
     <v-app>
         <v-navigation-drawer v-model="sidebar" app>
             <v-list>
-                <v-list-tile
+                <v-list-item
                     v-for="item in menuItems"
                     :key="item.title"
                     :to="item.path"
                 >
-                    <v-list-tile-action>
+                    <v-list-item-action>
                         <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-                </v-list-tile>
+                    </v-list-item-action>
+                    <v-list-item-content>{{ item.title }}</v-list-item-content>
+                </v-list-item>
             </v-list>
         </v-navigation-drawer>
 
-        <v-toolbar app>
+        <v-app-bar>
             <span class="hidden-sm-and-up">
-                <v-toolbar-side-icon @click="sidebar = !sidebar">
-                </v-toolbar-side-icon>
+                <v-app-bar-nav-icon @click="sidebar = !sidebar">
+                </v-app-bar-nav-icon>
             </span>
-            <v-toolbar-title>
+            <v-app-bar-title>
                 <router-link to="/" tag="span" style="cursor: pointer">
                     {{ appTitle }}
                 </router-link>
-            </v-toolbar-title>
+            </v-app-bar-title>
             <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-xs-only">
-                <v-btn
-                    flat
-                    v-for="item in menuItems"
-                    :key="item.title"
-                    :to="item.path"
-                >
-                    <v-icon left dark>{{ item.icon }}</v-icon>
-                    {{ item.title }}
-                </v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
+            <v-btn
+                text
+                v-for="item in menuItems"
+                :key="item.title"
+                :to="item.path"
+            >
+                <v-icon left dark>{{ item.icon }}</v-icon>
+                {{ item.title }}
+            </v-btn>
+        </v-app-bar>
 
         <v-main>
             <router-view />
--- frontend-vue/src/views/auth/Login.vue
+++ frontend-vue/src/views/auth/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app id="inspire">
-        <v-content>
+        <v-main>
             <v-container fluid fill-height>
                 <v-layout align-center justify-center>
                     <v-flex xs12 sm8 md4>
@@ -11,12 +11,14 @@
                             <v-card-text>
                                 <v-form>
                                     <v-text-field
+                                        v-model="credentials.email"
                                         prepend-icon="person"
                                         name="login"
                                         label="Login"
                                         type="text"
                                     ></v-text-field>
                                     <v-text-field
+                                        v-model="credentials.password"
                                         id="password"
                                         prepend-icon="lock"
                                         name="password"
@@ -27,13 +29,15 @@
                             </v-card-text>
                             <v-card-actions>
                                 <v-spacer></v-spacer>
-                                <v-btn color="primary" to="/">Login</v-btn>
+                                <v-btn color="primary" @click="fireLogin"
+                                    >Login</v-btn
+                                >
                             </v-card-actions>
                         </v-card>
                     </v-flex>
                 </v-layout>
             </v-container>
-        </v-content>
+        </v-main>
     </v-app>
 </template>
 
@@ -42,6 +46,19 @@ export default {
     name: "Login",
     props: {
         source: String,
+    },
+    data() {
+        return {
+            credentials: {
+                email: "",
+                password: "",
+            },
+        };
+    },
+    methods: {
+        fireLogin() {
+            console.log(this.credentials);
+        },
     },
 };
 </script>
