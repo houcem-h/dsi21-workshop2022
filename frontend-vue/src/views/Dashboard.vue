9d3e180a71460481e1f870b9048607b6dd245cb4
houcem <hedhoucem@gmail.com>
2022-05-22 18:22:48 +0100

    fetch posts in backend

--- frontend-vue/src/views/Dashboard.vue
+++ frontend-vue/src/views/Dashboard.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script>
+import "@/store/index";
 export default {
     name: "Dashboard",
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            this.$store.dispatch("fetchPosts").then((response) => {
+                this.posts = this.$store.getters.posts;
+            });
+        },
+    },
 };
 </script>
