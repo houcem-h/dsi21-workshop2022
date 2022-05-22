0590cb111fe0daea3369b1f6303079e5e051dc6e
houcem <hedhoucem@gmail.com>
2022-05-22 18:01:19 +0100

    login + store

--- frontend-vue/src/plugins/axios.js
+++ frontend-vue/src/plugins/axios.js
@@ -2,6 +2,7 @@
 
 import Vue from "vue";
 import axios from "axios";
+import store from "./../store";
 
 // Full config:  https://github.com/axios/axios#request-config
 axios.defaults.baseURL =
@@ -21,6 +22,10 @@ const _axios = axios.create(config);
 _axios.interceptors.request.use(
     function (config) {
         // Do something before request is sent
+        const token = store.getters.token;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
         return config;
     },
     function (error) {
